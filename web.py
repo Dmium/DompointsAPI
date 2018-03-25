b177da7bb21d6eb8bcd57ef83a1ae8aa7b1c1a8a
dmium <dominiczy.hall@gmail.com>
2018-03-25 02:29:18 +0100

    Basic test api

---
+++ web.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+from random import randint
+@app.route('/')
+def hello_world():
+    return 'Hello, World!' + str(randint(0, 10000))
+
+@app.route('/api/test/')
+def hello_world_test():
+    return jsonify([{"dat": 'Hello, World!' + str(randint(0, 10000))}])
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
