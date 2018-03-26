3c9d115239d18be355e0f916fc5222be47af63e8
dmium <dominiczy.hall@gmail.com>
2018-03-26 09:15:44 +0100

    json pls

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def hello_world_test():
 
 @app.route('/api/leaderboard')
 def get_leaderboard():
-    return "[  { username: 'Mod', dom_points: 44 },  { username: 'Dom', dom_points: 45 },  { username: 'Some other guy', dom_points: 25 },  { username: 'Some guy', dom_points: 20 },  { username: 'Me', dom_points: 4 },]"
+    return jsonify("[  { username: 'Mod', dom_points: 44 },  { username: 'Dom', dom_points: 45 },  { username: 'Some other guy', dom_points: 25 },  { username: 'Some guy', dom_points: 20 },  { username: 'Me', dom_points: 4 },]")
 if __name__ == '__main__':
     app.debug = True
     port = int(os.environ.get("PORT", 5000))
